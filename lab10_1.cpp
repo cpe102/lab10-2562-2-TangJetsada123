4f368069af9107d65291420668dfdddb2dd78a75
lab314-17@labeng.local <lab314-17@labeng.local>
2020-01-31 17:29:37 +0700

    Lab10 ver1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,48 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+    cout <<"Press Enter 3 times to reveal your future.";
+    cin.ignore();
+    cin.ignore();
+    cin.ignore();
+    int x;
+    string i;
+    srand(time(0));
+    x = rand()%8;
+    
+    switch(x)
+    {
+        case 1 :
+           i="A";
+           break;
+        case 2 :
+           i="B+";
+           break;
+        case 3 :
+           i="B";
+           break;
+        case 4 :
+           i="C+";
+           break;
+        case 5 :
+           i="C";
+           break;
+        case 6 :
+           i="D+";
+           break;
+        case 7 :
+           i="D";
+           break;
+        case 8 :
+           i="F";
+           break;
+        case 9 :
+           i="W";
+           break;
+    }
+    cout <<"You will get "<<i<< " in this 261102";
+}
